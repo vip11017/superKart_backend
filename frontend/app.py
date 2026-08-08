99c499f0a2adf96bc5c725a5498506ac5e149f4d
Vishal Patel <120346989+vip11017@users.noreply.github.com>
2026-08-08 09:39:38 -0700

    Update frontend/app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,50 +1,40 @@
 
+import streamlit as st
+import requests
+st.title("SuperKart Sales Prediction")
 
-import numpy as np
-import joblib
-import pandas as pd
-from flask import Flask, request, jsonify
+# Input fields for product and store data
+Product_Weight = st.number_input("Product Weight", min_value=0.0, value=12.66)
+Product_Sugar_Content = st.selectbox("Product Sugar Content", ["Low Sugar", "Regular", "No Sugar"])
+Product_Allocated_Area = st.number_input("Product Allocated Area", min_value=0.0, value=1.0)
+Product_MRP = st.number_input("Product MRP", min_value=0.0, value=147.03)
+Store_Size = st.selectbox("Store Size", ["Small", "Medium", "High"])
+Store_Location_City_Type = st.selectbox("Store Location City Type", ["Tier 1", "Tier 2", "Tier 3"])
+Store_Type = st.selectbox("Store Type", ["Departmental Store", "Supermarket Type1", "Supermarket Type2"])
+Product_Id_Type = st.selectbox("Product ID Type", ["FD", "DR", "NC"])
+Store_Age_Years = st.number_input("Store Age in Years", min_value=0, value=10)
+Product_Type_Category = st.selectbox("Product Type Category", ["Perishables", "Non Perishables"])
 
-#Initialize Flask App
-app = Flask('superkart_app')
+product_data = {
+    "Product_Weight": Product_Weight,
+    "Product_Sugar_Content": Product_Sugar_Content,
+    "Product_Allocated_Area": Product_Allocated_Area,
+    "Product_MRP": Product_MRP,
+    "Store_Size": Store_Size,
+    "Store_Location_City_Type": Store_Location_City_Type,
+    "Store_Type": Store_Type,
+    "Product_Id_Type": Product_Id_Type,
+    "Store_Age_Years": Store_Age_Years,
+    "Product_Type_Category": Product_Type_Category,
+}
+#Add the 2 calculated fields to the product_data dictionary
+product_data["MRP_x_Area"] = product_data["Product_MRP"] * product_data["Product_Allocated_Area"]
+product_data["MRP_per_Area"] = product_data["Product_MRP"] / product_data["Product_Allocated_Area"]
 
-#load model 
-model = joblib.load("superkart_model.joblib")
-
-# Define the route for the API
-@app.get('/')
-def home():
-    return "Welcome to the SuperKart Sales Prediction API"
-
-# Endpoint to predict churn for a customer
-@app.post('/predict')
-def predict_sales():
-    data = request.get_json()
-
-    # Convert the input data into a DataFrame
-    sample = {
-        'Product_Weight': data['Product_Weight'],
-        'Product_Sugar_Content': data['Product_Sugar_Content'],
-        'Product_Allocated_Area': data['Product_Allocated_Area'],
-        'Product_MRP': data['Product_MRP'],
-        'Store_Size': data['Store_Size'],
-        'Store_Location_City_Type': data['Store_Location_City_Type'],
-        'Store_Type': data['Store_Type'],
-        'Product_ID_Type': data['Product_ID_Type'],
-        'Store_Age_Years': data['Store_Age_Years'],
-        'Product_Type_Category': data['Product_Type_Category'],
-        "MRP_x_Area" : data["Product_MRP"] * data["Product_Allocated_Area"],
-        "MRP_per_Area" : data["Product_MRP"] / data["Product_Allocated_Area"]
-    }
-
-    input_data = pd.DataFrame([sample])
-
-    #Make prediction
-    prediction = model.predict(input_data).tolist()[0]
-
-    #Return the prediction as a JSON response
-    return jsonify({"Sales": prediction})
-
-# Run the Flask app in debug mode
-if __name__ == '__main__':
-    app.run(debug=True)
+if st.button("Predict", type="primary"):
+    response = requests.post("https://vigilant-memory-w6gxw75j7wg2g7wq-7860.app.github.dev/predict", json=product_data)
+    if response.status_code == 200:
+        prediction = response.json()["Sales"]
+        st.success(f"Predicted Sales: ${prediction: .2f}")
+    else:
+        st.error("Error in prediction. Please try again.")
